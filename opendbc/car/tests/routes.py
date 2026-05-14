57c66897694f75be24e634d33256c7009d783355
DonGuaja <DonGuaja@users.noreply.github.com>
2026-05-13 22:31:57 -0400

    Update routes w/ cx9 eps swap

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -28,6 +28,7 @@ non_tested_cars = [
   SUBARU.SUBARU_FORESTER_HYBRID,
   VOLKSWAGEN.PORSCHE_MACAN_MK1,
   HONDA.ACURA_TLX_2G,
+  MAZDA.MAZDA_CX9_2021_EPS_CX5_2022,
 
   # These had their DSUs unplugged, need new routes
   # TOYOTA.LEXUS_ES  # hybrid
